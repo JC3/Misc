06a0fffcaf3d3da8c0c0ad31ef17239ecc9e0aa3
JasonC <jason.cipriani@gmail.com>
2014-11-19 13:19:29 -0500

    Fixed bug where script crashed on own comments.
    Script crashed on own comments, which do not have a flag button and do
    not have a second table row.

--- scripts/SO Horizontal Comment Action.user.js
+++ scripts/SO Horizontal Comment Action.user.js
@@ -2,7 +2,7 @@
 // @name         SO Horizontal Comment Action
 // @author       Jason C
 // @description  Put comment flag icon next to vote icon instead of below it.
-// @version      0.3
+// @version      0.4
 // @namespace    
 // @include http*://*.stackexchange.com*
 // @include http*://*.stackoverflow.com*
@@ -22,11 +22,12 @@ for (var n = 0; n < actions.length; ++ n) {
     
     // find second td of second tr (the flag icon), move it to first tr (next to vote action),
     // then remove second tr (now unused).
-    
     var parent = actions[n].getElementsByTagName('tbody')[0];
     var rows = parent.getElementsByTagName('tr');
     var dsttr = rows[0];
     var srctr = rows[1];
+    if (!srctr) // your own comments won't have this row (you can't flag them).
+        continue;
     var srctd = srctr.getElementsByTagName('td')[1];
     
     srctr.removeChild(srctd);
